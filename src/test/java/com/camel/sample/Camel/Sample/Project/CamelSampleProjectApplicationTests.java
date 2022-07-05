14146aacfad897f4de00c15dd3483313e47de32f
beyzaydin <beyzaaydin@etu.edu.tr>
2022-07-05 21:02:26 +0300

    repository initialize commit

---
+++ src/test/java/com/camel/sample/Camel/Sample/Project/CamelSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.sample.Camel.Sample.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
